1200532c1a59325cf0da78ec4d512e62beec4cb4
shreyashmanglam-hash <shreyashmanglam@gmail.com>
2025-12-13 17:14:06 +0530

    Add EvenOdd

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class EvenOdd {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num,odd=0,even=0;
+        System.out.println("Enter 10 numbers: ");
+        for (int i=0;i<10;i++) {
+            num = sc.nextInt();
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.println("Even: "+even);
+        System.out.println("Odd: "+odd);
+            }
+        }
+
+
